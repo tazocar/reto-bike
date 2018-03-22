234e9176ab6c32dd3ff185e1d7cca8a37103cf95
hobilin <paulibareyes@gmail.com>
2018-03-22 11:07:21 -0300

    mapbox mas marcadores para arreglar

---
+++ src/Components/Map.js
@@ -0,0 +1,104 @@
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+import mapboxgl from 'mapbox-gl'
+
+mapboxgl.accessToken = 'pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4M29iazA2Z2gycXA4N2pmbDZmangifQ.-g_vE53SD2WrJ6tFX7QHmA';
+
+var geojson = {
+    "type": "FeatureCollection",
+    "features": [
+        {
+            "type": "Feature",
+            "properties": {
+                "message": "Foo",
+                "iconSize": [60, 60]
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -66.324462890625,
+                    -16.024695711685304
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "message": "Bar",
+                "iconSize": [50, 50]
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -61.2158203125,
+                    -15.97189158092897
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "message": "Baz",
+                "iconSize": [40, 40]
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -63.29223632812499,
+                    -18.28151823530889
+                ]
+            }
+        }
+    ]
+};
+
+
+class Application extends Component {
+
+  componentDidMount() {
+
+    // Container to put React generated content in.
+
+    const map = new mapboxgl.Map({
+      container: this.mapContainer,
+      style: 'mapbox://styles/mapbox/streets-v9',
+      center: [-65.017, -16.457],
+      zoom: 5
+    });
+
+// add markers to map
+geojson.features.forEach(function(marker) {
+
+  fetch('https://api.citybik.es/v2/networks/santiago')
+  .then(data => data.json())
+  .then(response => {
+    //console.log(response.network.stations.map(item => console.log('het')))
+    console.log(response.network.stations)
+  })
+    // create a DOM element for the marker
+    var el = document.createElement('div');
+    el.className = 'marker';
+    el.style.backgroundImage = 'url(https://placekitten.com/g/' + marker.properties.iconSize.join('/') + '/)';
+    el.style.width = marker.properties.iconSize[0] + 'px';
+    el.style.height = marker.properties.iconSize[1] + 'px';
+
+    el.addEventListener('click', function() {
+        window.alert(marker.properties.message);
+    });
+
+    // add marker to map
+    new mapboxgl.Marker(el)
+        .setLngLat(marker.geometry.coordinates)
+        .addTo(map);
+});
+
+  }
+
+  render() {
+    return (
+      <div ref={el => this.mapContainer = el} className="absolute top right left bottom" />
+    );
+  }
+}
+
+export default Application;
