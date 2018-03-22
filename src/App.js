5287ad4cfc57a4cdcf81c09cd851a76f4f1ed5dc
tazocar <tankodani@gmail.com>
2018-03-22 19:22:37 -0300

    mapa solo al login

--- src/App.js
+++ src/App.js
@@ -10,17 +10,14 @@ class App extends Component {
     this.handleAuth = this.handleAuth.bind(this)
     this.handleLogout = this.handleLogout.bind(this)
   }
-
   state = {
     user: null
   }
-
   componentWillMount() {
     firebase.auth().onAuthStateChanged(user => {
       this.setState({ user })
     })
   }
-
   handleAuth() {
     const provider = new firebase.auth.GoogleAuthProvider()
     provider.addScope('https://www.googleapis.com/auth/plus.login')
@@ -29,23 +26,38 @@ class App extends Component {
       .then(result => console.log(`${result.user.email} ha iniciado sesión`))
       .catch(error => console.log(`Error ${error.code}: ${error.message}`))
   }
-
   handleLogout() {
     firebase.auth().signOut()
       .then(result => console.log('Te has desconectado correctamente'))
       .catch(error => console.log(`Error ${error.code}: ${error.message}`))
   }
+  renderMap(){
+    if(this.state.user){
+      return(
+        <div className="App">
+          <Login
+            user={this.state.user}
+            onAuth={this.handleAuth}
+            onLogout={this.handleLogout}
+          />
+          <Application/>
+        </div>
+      );
+    }else{
+      return(
+        <Login
+            user={this.state.user}
+            onAuth={this.handleAuth}
+            onLogout={this.handleLogout}
+          />
+      );
+    }
+  }
   render() {
     return (
       <div className="App">
-        <Login
-          user={this.state.user}
-          onAuth={this.handleAuth}
-          onLogout={this.handleLogout}
-        />
-        <Application/>
+        { this.renderMap() }
       </div>
-      
     );
   }
 }
